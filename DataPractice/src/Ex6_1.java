176712ca19dea1c024a6fa44e0be5b8e1fb83e75
Hyun-ju Cho <chohj377@gmail.com>
2017-05-22 15:37:50 +0900

    주석 처리 추가1

--- DataPractice/src/Ex6_1.java
+++ DataPractice/src/Ex6_1.java
@@ -1,33 +1,23 @@
-
-public class Ex6_1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList L = new LinkedList();
-		System.out.println("(1) 공백 리스트에 노드 3개 삽입하기");
-		L.insertLastNode("월");
-		L.insertLastNode("수");
-		L.insertLastNode("일");
-		L.printList();
-		
-		System.out.println("(2) 수 노드 뒤에 금 노드 삽입하기");
-		ListNode pre = L.searchNode("수");
-		if(pre==null)
-			System.out.println("검색실패 >> 찾는 데이터가 없습니다.");
-		else{
-			L.insertMiddleNode(pre,"금");
-			L.printList();
-		}
-		
-		System.out.println("(3) 리스트의 노드를 역순으로 바꾸기");
-		L.reverseList();
-		L.printList();
-		
-		System.out.println("(4) 리스트의 마지막 노드 삭제하기");
-		L.deleteLastNode();
-		L.printList();
+//노드 클래스 정의
+class ListNode{
+	private String data;
+	public ListNode link;
+	//overloading..polymorphism
+	public ListNode(){
+		this.data = null;
+		this.link = null;
 	}
-
+	public ListNode(String data){
+		this.data = data;
+		this.link = null;
+	}
+	public ListNode(String data, ListNode link){
+		this.data = data;
+		this.link = link;
+	}
+	public String getData(){
+		return this.data;
+	}
 }
 
 class LinkedList{
@@ -38,8 +28,14 @@ class LinkedList{
 	}
 	public void insertMiddleNode(ListNode pre, String data){
 		ListNode newNode = new ListNode(data);
-		newNode.link = pre.link;
-		pre.link = newNode;
+		if(head==null){
+			this.head = newNode;
+		}
+		else{
+			newNode.link = pre.link;
+			pre.link = newNode;
+		}
+		
 	}
 	public void insertLastNode(String data){
 		ListNode newNode = new ListNode(data);
@@ -55,11 +51,14 @@ class LinkedList{
 	
 	public void deleteLastNode(){
 		ListNode pre, temp;
+		//아무것도 없을 때
 		if(head==null) return;
+		//마지막 노드일 때
 		if(head.link == null){
 			head=null;
 		}
 		else{
+			//로직이 잘 이해가지 않음
 			pre = head;
 			temp = head.link;
 			while(temp.link != null){
@@ -105,23 +104,35 @@ class LinkedList{
 	}
 }
 
-class ListNode{
-	private String data;
-	public ListNode link;
-	
-	public ListNode(){
-		this.data = null;
-		this.link = null;
+
+public class Ex6_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList L = new LinkedList();
+		System.out.println("(1) 공백 리스트에 노드 3개 삽입하기");
+		L.insertLastNode("월");
+		L.insertLastNode("수");
+		L.insertLastNode("일");
+		L.printList();
+		
+		System.out.println("(2) 수 노드 뒤에 금 노드 삽입하기");
+		ListNode pre = L.searchNode("수");
+		if(pre==null)
+			System.out.println("검색실패 >> 찾는 데이터가 없습니다.");
+		else{
+			L.insertMiddleNode(pre,"금");
+			L.printList();
+		}
+		
+		System.out.println("(3) 리스트의 노드를 역순으로 바꾸기");
+		L.reverseList();
+		L.printList();
+		
+		System.out.println("(4) 리스트의 마지막 노드 삭제하기");
+		L.deleteLastNode();
+		L.printList();
 	}
-	public ListNode(String data){
-		this.data = data;
-		this.link = null;
-	}
-	public ListNode(String data, ListNode link){
-		this.data = data;
-		this.link = link;
-	}
-	public String getData(){
-		return this.data;
-	}
+
 }
+
